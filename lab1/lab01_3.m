c8f0a6a86ce6697995fbfddd2c6dbfa2e7daaa0b
Jan Bronicki <janbronicki@gmail.com>
2022-03-23 21:24:54 +0100

    gbfdsfgnbhf

--- lab1/lab01_3.m
+++ lab1/lab01_3.m
@@ -1,23 +1,17 @@
-function lab01_3
-    % Warunki początkowe
-    x0 = [0 1];
-    % Sterowanie
-    u = 5;
+function Lab01
+    % Warunek poczatkowy
+    x0 = 0;
     % Plotujemy wykres od 0 do 10
     tspan = [0 10];
     % Rozwiązanie
-    [tsol, xsol] = ode45(@(t, x)ode1(t, x, u), tspan, x0);
+    [tsol, xsol] = ode45(@ode1, tspan, x0);
     % Wyrysowanie wykresu
     plot(tsol, xsol)
     ylabel("x");
     xlabel("t");
-    legend("x_{1}", "x_{2}");
-    title("Wykres wartości x_{1} i x_{2} od parametru t")
     grid on;
 end
 
-function dxdt = ode1(t, x, u)
-    dxdt = zeros(2, 1);
-    dxdt(1) = x(2) + t + u;
-    dxdt(2) = x(1) + x(2);
+function dtdx = ode1(~, ~)
+    dtdx = 5;
 end
