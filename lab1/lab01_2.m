e2869dacd71a3c941e4df566f42ee680f4f39b24
Jan Bronicki <janbronicki@gmail.com>
2022-03-23 21:19:50 +0100

    sdfg

--- lab1/lab01_2.m
+++ lab1/lab01_2.m
@@ -1,24 +1,21 @@
 function Lab01
-% Warunki poczatkowe
-x0 = [0 1];
-% Plotujemy wykres od 0 do 10
-tspan = [0 10];
-% Rozwiazanie
-[tsol,xsol]=ode45(@ode1,tspan,x0);
-% Wyrysowanie wykresu
-plot(tsol,xsol);
-ylabel("x");
-xlabel("t");
-legend("x_{1}","x_{2}");
-title("Wykres wartości x_{1} i x_{2} od parametru t")
-grid on;
+    % Warunki poczatkowe
+    x0 = [0 1];
+    % Plotujemy wykres od 0 do 10
+    tspan = [0 10];
+    % Rozwiazanie
+    [tsol, xsol] = ode45(@ode1, tspan, x0);
+    % Wyrysowanie wykresu
+    plot(tsol, xsol);
+    ylabel("x");
+    xlabel("t");
+    legend("x_{1}", "x_{2}");
+    title("Wykres wartości x_{1} i x_{2} od parametru t")
+    grid on;
 end
 
-function dxdt=ode1(t,x)
-% 
-dxdt=zeros(2,1);
-% 
-dxdt(1)=x(2)+t;
-% 
-dxdt(2)=x(1)+x(2);
+function dxdt = ode1(t, x)
+    dxdt = zeros(2, 1);
+    dxdt(1) = x(2) + t;
+    dxdt(2) = x(1) + x(2);
 end
--- lab1/lab01_3.m
+++ lab1/lab01_3.m
@@ -1,14 +1,23 @@
 function lab01_3
-x0 = [0 1];
-u = 5;
-tspan = [0 10]; %mamy jedno 0 ale 1 warunek ????
-[tsol,xsol]=ode45(@(t,x)ode1(t,x,u),tspan,x0);
-plot(tsol,xsol)
+    % Warunki początkowe
+    x0 = [0 1];
+    % Sterowanie
+    u = 5;
+    % Plotujemy wykres od 0 do 10
+    tspan = [0 10];
+    % Rozwiązanie
+    [tsol, xsol] = ode45(@(t, x)ode1(t, x, u), tspan, x0);
+    % Wyrysowanie wykresu
+    plot(tsol, xsol)
+    ylabel("x");
+    xlabel("t");
+    legend("x_{1}", "x_{2}");
+    title("Wykres wartości x_{1} i x_{2} od parametru t")
+    grid on;
 end
 
-function dxdt=ode1(t,x, u) %p oraz x
-dxdt=zeros(2,1);
-dxdt(1)=x(2)+t + u;
-dxdt(2)=x(1)+x(2);
-%dxdt=5
+function dxdt = ode1(t, x, u)
+    dxdt = zeros(2, 1);
+    dxdt(1) = x(2) + t + u;
+    dxdt(2) = x(1) + x(2);
 end
